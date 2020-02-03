ee18580b22fdd39f29d210b21d2ab5ca9e2f9a01
Kristin Barr <kristinbarr@gmail.com>
2020-02-03 00:03:49 -0500

    initial functions no explainers

---
+++ index.js
@@ -0,0 +1,113 @@
+function getAllElementsButNth(array, n) {
+}
+
+function getElementsThatEqual10AtProperty(obj, key) {
+}
+
+function select(arr, obj) {
+}
+
+function getElementsLessThan100AtProperty(obj, key) {
+}
+
+function getElementsGreaterThan10AtProperty(obj, key) {
+}
+
+function removeElement(array, discarder) {
+}
+
+function keep(array, keeper) {
+}
+
+function getOddLengthWordsAtProperty(obj, key) {
+}
+
+function computeAverageOfNumbers(nums) {
+}
+
+function getAverageOfElementsAtProperty(obj, key) {
+}
+
+function getEvenLengthWordsAtProperty(obj, key) {
+}
+
+function filterOddLengthWords(words) {
+}
+
+function getSquaredElementsAtProperty(obj, key) {
+}
+
+function getOddElementsAtProperty(obj, key) {
+}
+
+function getEvenElementsAtProperty(obj, key) {
+}
+
+function filterEvenLengthWords(words) {
+}
+
+function getLengthOfLongestElement(arr) {
+}
+
+function getSmallestElementAtProperty(obj, key) {
+}
+
+function getLargestElementAtProperty(obj, key) {
+}
+
+function squareElements(arr) {
+}
+
+function filterOddElements(arr) {
+}
+
+function computeProductOfAllElements(arr) {
+}
+
+function filterEvenElements(arr) {
+}
+
+function getLengthOfShortestElement(arr) {
+}
+
+function getLongestElement(arr) {
+}
+
+function findSmallestElement(arr) {
+}
+
+function findShortestElement(arr) {
+}
+
+function getLargestElement(arr) {
+}
+
+function computeSumOfAllElements(arr) {
+}
+
+function getProductOfAllElementsAtProperty(obj, key) {
+}
+
+function sumDigits(num) {
+}
+
+function getSumOfAllElementsAtProperty(obj, key) {
+}
+
+function findShortestWordAmongMixedElements(arr) {
+}
+
+function findSmallestNumberAmongMixedElements(arr) {
+}
+
+function getLongestWordOfMixedElements(arr) {
+}
+
+function getLargestNumberAmongMixedElements(arr) {
+}
+
+function getLongestOfThreeWords(word1, word2, word3) {
+}
+
+function findShortestOfThreeWords(word1, word2, word3) {
+}
