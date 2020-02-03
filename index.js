f86b15ffdc6b505d101c79cddab5e520c8a88e9e
Kristin Barr <kristinbarr@gmail.com>
2020-02-03 18:59:27 -0500

    more question descriptions

--- index.js
+++ index.js
@@ -108,41 +108,118 @@ function keep(arr, keeper) {
 }
 
 function getOddLengthWordsAtProperty(obj, key) {
+  // Given an object and a key, “getOddLengthWordsAtProperty” returns an array containing all the odd length word elements of the array located at the given key.
+
+  // Notes:
+  // * If the array is empty, it should return an empty array.
+  // * If it contains no odd length elements, it should return an empty array.
+  // * If the property at the given key is not an array, it should return an empty array.
+  // * If there is no property at the given key, it should return an empty array.
+
+  // var obj = {
+  //   key: ['It', 'has', 'some', 'words']
+  // };
+  // var output = getOddLengthWordsAtProperty(obj, 'key');
+  // console.log(output); // --> ['has', 'words']
 
   /* YOUR CODE HERE */
+
 }
 
 function computeAverageOfNumbers(nums) {
+  // Given an array of numbers, “computeAverageOfNumbers” returns their average.
+
+  // Notes:
+  // * If given an empty array, it should return 0.
+
+  // var input = [1,2,3,4,5];
+  // var output = computeAverageOfNumbers(input);
+  // console.log(output); // --> 3
 
   /* YOUR CODE HERE */
 }
 
 function getAverageOfElementsAtProperty(obj, key) {
+  // Given an object and a key, “getAverageOfElementsAtProperty” returns the average of all the elements in the array located at the given key.
+
+  // Notes:
+  // * If the array at the given key is empty, it should return 0.
+  // * If the property at the given key is not an array, it should return 0.
+  // * If there is no property at the given key, it should return 0.
+
+  // var obj = {
+  //   key: [1, 2, 3]
+  // };
+  // var output = getAverageOfElementsAtProperty(obj, 'key');
+  // console.log(output); // --> 2
 
   /* YOUR CODE HERE */
 }
 
 function getEvenLengthWordsAtProperty(obj, key) {
+  // Given an object and a key, “getEvenLengthWordsAtProperty” returns an array containing all the even length word elements of the array located at the given key.
+
+  // Notes:
+  // * If the array is empty, it should return an empty array.
+  // * If it contains no even length elements, it should return an empty array.
+  // * If the property at the given key is not an array, it should return an empty array.
+  // * If there is no property at the key, it should return an empty array.
+
+  // var obj = {
+  //   key: ['a', 'long', 'game']
+  // };
+  // var output = getEvenLengthWordsAtProperty(obj, 'key');
+  // console.log(output); // --> ['long', 'game']
 
   /* YOUR CODE HERE */
 }
 
 function filterOddLengthWords(words) {
+  // Given an array of string, “filterOddLengthWords” returns an array containing only the elements of the given array whose lengths are odd numbers.
+
+  // var output = filterOddLengthWords(['there', 'it', 'is', 'now']);
+  // console.log(output); // --> ['there', "now']
 
   /* YOUR CODE HERE */
 }
 
 function getSquaredElementsAtProperty(obj, key) {
+  // Given an object and a key, “getSquaredElementsAtProperty” returns an array containing all the squared elements of the array located at the given key.
+
+  // Notes:
+  // * If the array is empty, it should return an empty array.
+  // * If the property at the given key is not an array, it should return an empty array.
+  // * If there is no property at the key, it should return an empty array.
+
+  // var obj = {
+  //   key: [2, 1, 5]
+  // };
+  // var output = getSquaredElementsAtProperty(obj, 'key');
+  // console.log(output); // --> [4, 1, 25]
 
   /* YOUR CODE HERE */
 }
 
 function getOddElementsAtProperty(obj, key) {
+  // Given an object and a key, “getOddElementsAtProperty” returns an array containing all the odd elements of the array located at the given key.
+
+  // Notes:
+  // * If the array is empty, it should return an empty array.
+  // * If it contains no odd elements, it should return an empty array.
+  // * If the property at the given key is not an array, it should return an empty array.
+  // * If there is no property at the key, it should return an empty array.
+
+  // var obj = {
+  //   key: [1, 2, 3, 4, 5]
+  // };
+  // var output = getOddElementsAtProperty(obj, 'key');
+  // console.log(output); // --> [1, 3, 5]
 
   /* YOUR CODE HERE */
 }
 
 function getEvenElementsAtProperty(obj, key) {
+
 
   /* YOUR CODE HERE */
 }
